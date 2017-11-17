d705a18ba5d5cb4d5b13f9f18071c7c2282f868b
Harish <harish@Harishs-Mac-mini.local>
2017-11-17 11:41:00 +0530

    upload

---
+++ HarishFrameworkSwift3.framework/Headers/HarishFrameworkSwift3.h
@@ -0,0 +1,19 @@
+//
+//  HarishFrameworkSwift3.h
+//  HarishFrameworkSwift3
+//
+//  Created by Harish on 16/11/17.
+//  Copyright © 2017 Harish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HarishFrameworkSwift3.
+FOUNDATION_EXPORT double HarishFrameworkSwift3VersionNumber;
+
+//! Project version string for HarishFrameworkSwift3.
+FOUNDATION_EXPORT const unsigned char HarishFrameworkSwift3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HarishFrameworkSwift3/PublicHeader.h>
+
+
